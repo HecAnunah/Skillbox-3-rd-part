befd7cb96865d6ba0351094b99f2e64fc66b46e5
Zakhar <pkmaster2@gmail.com>
2025-07-03 19:08:07 +0300

    'first advansed lesson is create 03.07.2025'

---
+++ 1. Flask/1.py
@@ -0,0 +1,15 @@
+"""
+1. Переходим в консоли в папку с проектом на FLASK */first_applicaton
+2. Пишем строки:
+    export FLASK_APP="app.py"
+    export FLASK_DEBUG=1
+    flask run
+        ИЛИ: python -m flask run port=5555
+3. Получаем такой ответ:
+ * Serving Flask app 'app.py'
+ * Debug mode: on
+WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
+ * Running on http://127.0.0.1:5000 - это адрес нашего тестового сервера. Можно дописать /test
+Press CTRL+C to quit
+ * Restarting with stat
+"""
--- Practic/module_01_flask/homework/app.py
+++ Practic/module_01_flask/homework/app.py
@@ -1,43 +1,84 @@
 import datetime
+import random
 from flask import Flask
+import os
+import re
 
 app = Flask(__name__)
 
+cars = ["Chevrolet", "Renault", "Ford", "Lada"]
+cats = [
+    "корниш-рекс",
+    "русская голубая",
+    "шотландская вислоухая",
+    "мейн-кун",
+    "манчкин",
+]
 
-@app.route('/hello_world')
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BOOK_FILE = os.path.join(BASE_DIR, "war_and_peace.txt")
+with open(BOOK_FILE, "r", encoding="utf-8") as f:
+    text = f.read()
+words = re.findall(r"\b\w+\b", text, flags=re.UNICODE)
+
+
+class Counter:
+    def __init__(self):
+        self.counter = 0
+
+    def visits(self):
+        self.counter += 1
+        return self.counter
+
+
+counter = Counter()
+
+
+@app.route("/hello_world")
 def test_function():
-    pass
+    return f"Hello, world!"
 
 
-@app.route('/cars')
-def test_function():
-    pass
+@app.route("/cars")
+def test_cars():
+    global cars
+    return f"Cars new pages in brows. We have a new cars, brands: {', '.join(cars)}."
 
 
-@app.route('/cats')
-def test_function():
-    pass
+@app.route("/cats")
+def test_cats():
+    global cats
+    choise_response = random.choice(cats)
+    return f"Вы больше всего соответствуете: {choise_response}"
 
 
-@app.route('/get_time/now')
-def test_function():
-    pass
+@app.route("/get_time/now")
+def test_get_time():
+    time = datetime.datetime.now()
+    return "Время сейчас: " + str(time)
 
 
-@app.route('/get_time/future')
-def test_function():
-    pass
+@app.route("/get_time/future")
+def test_future_time():
+    time_now = datetime.datetime.now()
+    current_time_after_hour = time_now + datetime.timedelta(hours=1)
+    return f"Точное время через час будет: {current_time_after_hour}"
 
 
-@app.route('/get_random_word')
-def test_function():
-    pass
+@app.route("/get_random_word")
+def test_get_random_word():
+    global words
+    random_word = random.choice(words)
+    return f"Рандомное слово из книги Льва Толстого: {random_word}"
 
 
-@app.route('/counter')
-def test_function():
-    pass
+@app.route("/counter")
+def test_counter():
+    global counter
+    count = counter.visits()
+    return str(count)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
