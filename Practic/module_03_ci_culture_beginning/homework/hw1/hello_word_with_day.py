b9dd13fd28438c3ff676d62c70dc681b9131eae5
Zakhar <pkmaster2@gmail.com>
2025-07-16 15:02:22 +0300

    'new hw1 is compl 16.07.2025'

--- Practic/module_03_ci_culture_beginning/homework/hw1/hello_word_with_day.py
+++ Practic/module_03_ci_culture_beginning/homework/hw1/hello_word_with_day.py
@@ -5,22 +5,24 @@ from flask import Flask
 app = Flask(__name__)
 
 GREETINGS = (
-    'Хорошего понедельника',
-    'Хорошего вторника',
-    'Хорошей среды',
-    'Хорошего четверга',
-    'Хорошей пятницы',
-    'Хорошей субботы',
-    'Хорошего воскресенья'
+    "Хорошего понедельника",
+    "Хорошего вторника",
+    "Хорошей среды",
+    "Хорошего четверга",
+    "Хорошей пятницы",
+    "Хорошей субботы",
+    "Хорошего воскресенья",
 )
 
 
-@app.route('/hello-world/<name>')
+@app.route("/hello-world/<name>")
 def hello_world(name: str) -> str:
+    if len(name.split()) > 1:
+        return "Имя должно быть односоставным", 400
     weekday: int = datetime.today().weekday()
     greeting: str = GREETINGS[weekday]
-    return f'Привет, {name}. {greeting}!'
+    return f"Привет, {name}. {greeting}!"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
