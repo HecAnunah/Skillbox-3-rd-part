c7e2e722da39bf8b782eec717c941801c48b3ec2
Zakhar <pkmaster2@gmail.com>
2025-08-04 23:01:51 +0300

    'new module-8 is complited 04.08.2025'

--- test/redirect.py
+++ test/redirect.py
@@ -1,13 +1,15 @@
-import logging
-
-class DebugHandler(logging.Handler):
-    def emit(self, record: logging.LogRecord) -> None:
-        print("LogRecord содержит атрибуты:")
-        for key, value in vars(record).items():
-            print(f"{key}: {value}")
-
-logger = logging.getLogger("example")
-logger.setLevel(logging.DEBUG)
-logger.addHandler(DebugHandler())
-
-logger.debug("Test", extra={"user_id": 42})
+a = 1
+try:
+    a = 2
+    print(a)
+    raise Exception()
+except:
+    a = 3
+    print(a)
+    raise
+else:
+    a = 4
+    print(a)
+finally:
+    a = 5
+    print(a)
